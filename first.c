107f7d73db2022d41b7d111737e76a4a407b9197
krishna0231 <krishnasaud864@gmail.com>
2024-12-07 04:31:40 +0000

    day 1 work

---
+++ first.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+
+int main(){
+    printf("Hello devs i am krishna");
+}
---
+++ third.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    int i;
+    printf("Enter any number");
+    scanf("%d",&i);
+    printf("The entered number by the userr is %d",i);
+}
