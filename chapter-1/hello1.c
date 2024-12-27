5c539b02008dcdb778995435da9a4b0b6e90de5b
krishna saud <krishnasaud864@gmail.com>
2024-12-27 20:39:42 +0545

    sample of area of a square of side a

---
+++ chapter-1/hello1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+    int a;
+    printf("Enter a");
+    scanf("%d", &a)'
+    //area of spuare is a*a
+    printf("arera of square is : a*a");
+}
