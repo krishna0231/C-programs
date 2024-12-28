2bdb34bd7802fd662d4260cde04819a3bcf0e600
krishna saud <krishnasaud864@gmail.com>
2024-12-28 17:26:04 +0545

    day2

--- basics/second.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main(){
-    int i;
-    printf("Enter any number");
-    scanf("%d",&i);
-    printf("The entered number by the userr is %d",i);
-}
--- chapter-1/hello1.c
+++ chapter-1/hello1.c
@@ -2,9 +2,8 @@
 #include<conio.h>
 
 void main(){
-    int a=4;
+    int a;
     printf("Enter a");
     scanf("%d", &a);
-    //area of spuare is a*a
     printf("arera of square is : a*a");
 }
