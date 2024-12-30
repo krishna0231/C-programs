a3af77834dc4593470c3df077f22eb9b1c5c8704
krishna saud <krishnasaud864@gmail.com>
2024-12-30 22:14:13 +0545

    day 3 ,area of circle

---
+++ chapter-1/fifth.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+ //area oif circle
+ int main(){
+    float radius;
+printf("enter radius");
+scanf("%f", &radius);
+printf("area of square is %f",3.14*radius*radius);
+return 0;
+ }
