f3b9dabe3272f03e423dd95f278c5cb97f1ffddd
krishna saud <krishnasaud864@gmail.com>
2024-12-30 22:35:55 +0545

    area of rt angle

---
+++ chapter-1/seventh.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+ //area of right angle triangle
+ int main(){
+    float hight,base;
+printf("enter hight,base\n");
+scanf("%f", &hight,&base);
+printf("area of square is %f",0.5*hight*base);
+return 0;
+ }
