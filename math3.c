29671a5f7efe05838de1101742df7e66fc55f9ad
krishna saud <krishnasaud864@gmail.com>
2024-12-31 16:55:37 +0545

    %f=!%d

---
+++ math3.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+printf("%f", 2.0*30);
+}
--- maths.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main(){
-    int a=4*3/6*2;
-    printf("%d \n", a);
-    return 0;
-    
-}
