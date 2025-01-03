14d45d584ccc35d548d30ecfcfba0259e35a2f4b
krishna saud <krishnasaud864@gmail.com>
2025-01-03 10:36:17 +0545

    logical operation amd =&& ,or=||

---
+++ condi.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int issunday=1;
+    int ismonday=0;
+    printf("%d \n", issunday || ismonday);
+    printf("%d \n, issunday && ismonday");
+
+
+    return 0;
+
+}
