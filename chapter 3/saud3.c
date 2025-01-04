5b7fa51dcfa293563f6297770f9e3756f0c9d663
krishna saud <krishnasaud864@gmail.com>
2025-01-05 00:11:42 +0545

    sum n natural number

---
+++ chapter 3/saud3.c
@@ -0,0 +1,19 @@
+/*calculate sum of n natural number*/
+#include<stdio.h>
+
+int main(){
+    int n,i,sum;printf("enter a number");
+    scanf("%d",&n);
+    for(i=1;i=n;i++){
+        sum=sum+i;
+
+    printf("sum of first %d natural number is: %d\n",n,sum);
+    return 0;
+
+
+    }
+
+
+
+
+}
