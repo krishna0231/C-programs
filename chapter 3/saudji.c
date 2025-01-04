458a2b1a130724051d8f4588256c423059bbfdc9
krishna saud <krishnasaud864@gmail.com>
2025-01-04 23:31:25 +0545

    from cppt note

---
+++ chapter 3/saudji.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int r;
+    char g;
+    float p;
+    printf("enter your grade");
+    scanf("%c",&g);
+    printf("\nenter roll no..");
+    scanf("%d",&r);
+    printf("\enter your per.");
+    scanf("%f",&p);
+    printf("\nYour grade is %c\nYour roll is %d\nYour per is %f",g,r,p);
+return 0;
+}
