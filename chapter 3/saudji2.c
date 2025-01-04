7e543966d405ae567f69c21fd7aed242bc3f58f7
krishna saud <krishnasaud864@gmail.com>
2025-01-04 23:56:18 +0545

    a number is +ve or -ve

---
+++ chapter 3/saudji2.c
@@ -0,0 +1,15 @@
+/*finding a number +ve or -ve*/
+#include<stdio.h>
+
+int main(){
+    int num;
+    printf("enter a number");
+    scanf("%d",&num);
+    if (num>0){
+                printf("enter a number is positive");
+    }
+    else
+        printf("enter a number is negative");
+        return 0;
+
+}
