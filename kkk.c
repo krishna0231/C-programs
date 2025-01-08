3b448ad1690e60d496a0b158c6227b56dc058d40
Krishna saud <krishnasaud864@gmail.com>
2025-01-08 22:19:59 +0545

    product of two number

---
+++ kkk.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    float a,b,product;
+    printf("enter a");
+    scanf("%f",&a);
+    printf("enter b";)
+    scanf("%f",&b);
+    product=a*b;
+    printf("The product is: %f",product);
+    getch();
+    return 0;
+    
+}
