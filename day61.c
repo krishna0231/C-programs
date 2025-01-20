66d4075281922e63532a4e6dbf25df348bda9c0f
krishna saud <krishnasaud864@gmail.com>
2025-01-20 10:41:48 +0545

    ..

---
+++ day61.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+
+{
+    int a,b;
+    float c,d;
+    printf("enter value of a aand b:");
+    scanf("%d,%2d",&a,&b);
+    printf("value of a and b: %d %d",a,b);
+    getch();
+    
+}
