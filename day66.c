204f843de088ae8b9209b8f9b70cec5052d8025c
krishna saud <krishnasaud864@gmail.com>
2025-01-21 22:19:54 +0545

    sizeof opearator

---
+++ day66.c
@@ -0,0 +1,9 @@
+//sizeof operetor
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i; float f; char c;
+    printf("fint,float and char size are:%d%d%d",sizeof(i), sizeof(i),sizeof(f),sizeof(c) );
+    return 0;
+}
