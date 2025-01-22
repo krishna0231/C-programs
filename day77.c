d29408ba527bc4fec18d5fe7d60e3cad9fddc820
krishna saud <krishnasaud864@gmail.com>
2025-01-22 21:03:46 +0545

    charecter conversion

---
+++ day77.c
@@ -0,0 +1,13 @@
+//charecter conversion
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a=5,b=10,c;
+    c=a+b;
+    printf("the sum is:\t%X",c);
+    printf("\nthe sum is:\t%x",c);
+    printf("\n the sum is:\t%d",c);
+    printf("\nthe sum is:\t%o",c);
+    return 0;
+}
