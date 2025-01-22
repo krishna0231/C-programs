fe5fcfad5418b62478703be045b35eb5ce46f30b
krishna saud <krishnasaud864@gmail.com>
2025-01-22 20:49:45 +0545

    charectre conversion

---
+++ day76.c
@@ -0,0 +1,12 @@
+//charecter convversion
+//charecter conversion
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int num=31;
+    printf("num=%X",num);
+    getch();
+    return 0;
+    
+}
