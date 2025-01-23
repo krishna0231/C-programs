81c95f90c8246f08bb4b84f88a7fdcdc79dd51d5
krishna saud <krishnasaud864@gmail.com>
2025-01-23 10:50:42 +0545

    assingment ,increment and decrement operator

---
+++ assing.c
@@ -0,0 +1,17 @@
+//assignment operator
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+int a=10,b=5;
+b+=a; //b=b+a
+printf("\nb=%d",b);
+printf("\nb=%d",++b); //b=b+1 and a is return after addition
+printf("\nb=%d",--b); //b=b-1 and b is return after substraction
+printf("\nb=%d",b++); //first value return and then incremented or decremented
+printf("\nb=%d",b--);
+
+getch();
+return 0;
+
+}
