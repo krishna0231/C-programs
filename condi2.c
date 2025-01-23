da446053a0c9669f1fa554df3ff056253628b20f
krishna saud <krishnasaud864@gmail.com>
2025-01-23 11:20:59 +0545

    check odd or even by using conditional ooperators

---
+++ condi2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int num=7;
+    //Using conditional operator to check even or odd
+    (num %2==0) ? printf("%d is even\n", num) : ("%d is odd\n",num);
+    return 0;
+    
+}
