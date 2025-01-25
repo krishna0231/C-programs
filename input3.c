5135384537bf2470c9ecff3742e0b63dbaba2a8f
krishna saud <krishnasaud864@gmail.com>
2025-01-25 18:40:21 +0545

    different types of scanf() fynction

---
+++ input3.c
@@ -0,0 +1,16 @@
+// write a program to read multiple values of different types using single scanf() function
+#include<stdio.h>
+#include<conio.h>
+
+int main (){
+int in;
+float fn;
+char ch;
+cahr[10];
+printf(" enter an entegerf, float and string:,\n");
+scanf("%d %f %c %s", &in, &fn, &ch, &str);
+pvalues arintf("\nYour re : integer value =%d  float value=%f",in, fn);
+printf("\ncharecter value =%c and string value =%s",ch,str);
+getch();
+return 0;
+}
