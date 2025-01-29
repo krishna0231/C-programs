5246fcd14aa2b47d040cc522a05826ea2c335594
krishna saud <krishnasaud864@gmail.com>
2025-01-29 22:55:39 +0545

    unformatted function

---
+++ unfor2.c
@@ -0,0 +1,18 @@
+//write a program to read 
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char ch1,ch2;
+    printf("Eneter the first charecter:");
+    ch1=getch();
+    printf("enter the second charecter:");
+    ch2=getche();
+    printf("\nfirst charecter is:");
+    putch(ch1);
+    printf("\nsecond charecter is:");
+    putch(ch2);
+    getch();
+    return 0;
+    
+}
