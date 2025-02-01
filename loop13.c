f60bc0012af91cc655f3250c7e0d2c86dd1bc407
krishna saud <krishnasaud864@gmail.com>
2025-02-01 20:22:18 +0545

    write a certain pattern of series

---
+++ loop13.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the number n:");
+    scanf("%d",&n);
+    int i=0;
+    while(i<=n) {
+        printf("\n%d",i);
+        i++;
+    }
+    return 0;
+}
