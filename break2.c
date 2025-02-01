743f60c4384597efa61fb00479360f24e0e3bd2c
krishna saud <krishnasaud864@gmail.com>
2025-02-01 14:24:15 +0545

    break statement

---
+++ break2.c
@@ -0,0 +1,17 @@
+//display a number until odd is enterd  using break statement
+
+#include<stdio.h>
+int main()
+{
+    int n;
+ while(1)
+ {
+    printf("Enter a nmber:");
+    scanf("%d",&n);
+    if(n%2!=0);
+    break;
+
+
+ }
+ return 0;
+}
