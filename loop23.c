d6d41a9731e9afd1535f606e38c2a2b7f7b74e84
krishna saud <krishnasaud864@gmail.com>
2025-02-05 22:39:58 +0545

    break statement

---
+++ loop23.c
@@ -0,0 +1,17 @@
+// Breakn statement
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i;
+    for(i=1;i < 10;i++)
+    {
+        printf("\t%d", i);
+        if(i == 5); {
+        break; }
+        
+
+    }
+getch();
+return 0;
+}
