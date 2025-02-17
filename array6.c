6d1bb1b376eb60e38213ecbcf0ee2b0502bb48e3
krishna saud <krishnasaud864@gmail.com>
2025-02-17 07:35:48 +0545

    arrray from yt

---
+++ array6.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{ int i,j;
+    int marks[2][4]={{5,7,3,2},
+                    {8,6,76,12}};
+     for(int i=0;i<2;i++)
+     {
+        for(int j=0;j<4;j++)
+        {
+     }
+     printf("The values of %d,%d elements of the array is %d\n",i,marks[i]);
+   return 0;
+}
